1f8dbcb820c2abd3dda95037179ad5dfd1bf32f4
PrG <65294906+prgofficial@users.noreply.github.com>
2020-08-11 23:51:53 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -99,61 +99,6 @@ async def incoming_message_f(client, message):
             "**FCUK**! wat have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
-#
-async def incoming_gdrive_message_f(client, message):
-    """/gleech command"""
-    i_m_sefg = await message.reply_text("processing", quote=True)
-    is_zip = False
-    is_unzip = False
-    is_unrar = False
-    is_untar = False
-    if len(message.command) > 1:
-        if message.command[1] == "archive":
-            is_zip = True
-        elif message.command[1] == "unzip":
-            is_unzip = True
-        elif message.command[1] == "unrar":
-            is_unrar = True
-        elif message.command[1] == "untar":
-            is_untar = True
-    # get link from the incoming message
-    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "GLEECH")
-    LOGGER.info(dl_url)
-    LOGGER.info(cf_name)
-    if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        # start the aria2c daemon
-        aria_i_p = await aria_start()
-        LOGGER.info(aria_i_p)
-        current_user_id = message.from_user.id
-        # create an unique directory
-        new_download_location = os.path.join(
-            DOWNLOAD_LOCATION,
-            str(current_user_id),
-            str(time.time())
-        )
-        # create download directory, if not exist
-        if not os.path.isdir(new_download_location):
-            os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
-        # try to download the "link"
-        await call_apropriate_function_g(
-            aria_i_p,
-            dl_url,
-            new_download_location,
-            i_m_sefg,
-            is_zip,
-            cf_name,
-            is_unzip,
-            is_unrar,
-            is_untar,
-            message
-        )
-    else:
-        await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
-            f"<b>API Error</b>: {cf_name}"
-        )
 
 
 async def incoming_youtube_dl_f(client, message):
@@ -165,10 +110,10 @@ async def incoming_youtube_dl_f(client, message):
         message.reply_to_message, "YTDL"
     )
     LOGGER.info(dl_url)
-    if len(message.command) > 1:
-        if message.command[1] == "gdrive":
-            with open('blame_my_knowledge.txt', 'w+') as gg:
-                gg.write("I am noob and don't know what to do that's why I have did this")
+#    if len(message.command) > 1:
+#        if message.command[1] == "gdrive":
+#            with open('blame_my_knowledge.txt', 'w+') as gg:
+#                gg.write("I am noob and don't know what to do that's why I have did this")
     LOGGER.info(cf_name)
     if dl_url is not None:
         await i_m_sefg.edit_text("extracting links")
@@ -213,13 +158,13 @@ async def incoming_youtube_dl_f(client, message):
 async def g_yt_playlist(client, message):
     """ /pytdl command """
     #i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
-    if len(message.command) > 1:
-        if message.command[1] == "gdrive":
-            with open('blame_my_knowledge_again.txt', 'w+') as gg:
-                gg.write("I am noob and don't know what to do that's why I have did this")
-    if 'www.youtube.com/playlist' in message.reply_to_message.text:
-        i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
+#    if len(message.command) > 1:
+#        if message.command[1] == "gdrive":
+#            with open('blame_my_knowledge_again.txt', 'w+') as gg:
+#                gg.write("I am noob and don't know what to do that's why I have did this")
+    if 'youtube.com/playlist' in message.reply_to_message.text:
+        i_m_sefg = await message.reply_text("Downloading all videos from playlist. Please wait! 🥱", quote=True)
         await yt_playlist_downg(message.reply_to_message, i_m_sefg)
     
     else:
-        await i_m_sefg.edit_text("This command works for youtube playlists only!!")
+        await message.reply_text("This command works for youtube playlists only!!")
